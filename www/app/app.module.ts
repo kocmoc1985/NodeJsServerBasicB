fcf0e6db4989de71237b5ff40597aa35e0767c3b
Andrei Brassas <kocmoc1985@gmail.com>
2017-03-21 12:39:47 +0100

    v1.01 Train angular - DELL

--- www/app/app.module.ts
+++ www/app/app.module.ts
@@ -1,16 +1,16 @@
-    import { NgModule }      from '@angular/core';
-    import { BrowserModule } from '@angular/platform-browser';
-    import { FormsModule }   from '@angular/forms';
-    import { AppComponent }  from './app.component';
-    
-    @NgModule({
-      imports: [
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+
+@NgModule({
+    imports: [
         BrowserModule,
         FormsModule
-      ],
-      declarations: [
+    ],
+    declarations: [
         AppComponent
-      ],
-      bootstrap: [ AppComponent ]
-    })
-    export class AppModule { }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
